1abb245b21b28b655a6c61989929da69aa4a62b9
Jonathan <jonathantoledo11@gmail.com>
2021-11-10 11:00:57 -0800

    Submisson for deadline

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-// Your code here
+const apiURL = 'http://localhost:3000';
+ //GET /characters
+    
+async function getCharacter() {
+    const response = await fetch('http://localhost:3000/characters');
+    const data = await response.json();
+    console.log(data);
+}
+getCharacter();
+
